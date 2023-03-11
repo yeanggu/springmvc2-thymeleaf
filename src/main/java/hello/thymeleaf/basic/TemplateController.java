203147dbad705ee71418fa7e3ec4bd15c775a73a
yeeun <dpdms9621@naver.com>
2023-03-11 19:14:33 +0900

    [feat] 템플릿 레이아웃2

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -18,4 +18,9 @@ public class TemplateController {
         return "template/layout/layoutMain";
     }
 
+    @GetMapping("/layoutExtend")
+    public String layoutExtends() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
+
 }
